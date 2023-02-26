c24b487e59463eb076ca650f8ac94feff30a4317
ArturomcReyes <marco_reus.19@outlook.com>
2023-02-26 04:05:23 -0600

    Version inicial del proyecto project-jpa2

---
+++ src/main/java/com/arturoserver/project_jpa2/App.java
@@ -0,0 +1,13 @@
+package com.arturoserver.project_jpa2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
